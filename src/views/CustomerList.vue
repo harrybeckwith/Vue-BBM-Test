489854411ab5e1775cc990b9084dff7439d7f198
harrybeckwith <harryjbeckwith@gmail.com>
2021-01-07 16:29:13 +0000

    filter by customerID for quotes and policies

--- src/views/CustomerList.vue
+++ src/views/CustomerList.vue
@@ -1,15 +1,17 @@
 <template>
   <div>
     <h1>Search</h1>
-    <input type="text" placeholder="customer name" v-model="customerName" />
-    <button @click="search" :disabled="disableSearch">Search</button>
-    {{ customerName }}
-    {{ foundCustomer }}
-    <ul>
-      <li v-for="customer in customers">
-        {{ customer.name.first }} {{ customer.name.last }}
-      </li>
-    </ul>
+    <input
+      type="text"
+      placeholder="customer name"
+      v-model="searchedName"
+      @keyup="search"
+    />
+    <div v-if="selected.customer">
+      <p @click="getCustomerData">
+        {{ selected.customer.name.first }} {{ selected.customer.name.last }}
+      </p>
+    </div>
   </div>
 </template>
 
@@ -21,10 +23,16 @@ export default {
   data() {
     return {
       customers: [],
-      customerName: "",
-      firstName: "",
-      lastName: "",
-      foundCustomer: null,
+      searchedName: "",
+      name: {
+        first: "",
+        last: "",
+      },
+      selected: {
+        customer: null,
+        policy: null,
+        quote: null,
+      },
     };
   },
   created() {
@@ -39,23 +47,19 @@ export default {
         console.log(e);
       });
   },
-  computed: {
-    disableSearch() {
-      return !this.customerName.length > 0;
-    },
-  },
   methods: {
     formatSearchName() {
       // user input to array for first and second name
-      const nameToArr = this.customerName.split(" ");
+      const nameToArr = this.searchedName.split(" ");
       // check first or second is present
       if (nameToArr.length == 1) {
-        this.firstName = nameToArr[0].toLowerCase();
+        this.name.first = nameToArr[0].toLowerCase();
       } else {
-        this.lastName = nameToArr[1].toLowerCase();
+        this.name.last = nameToArr[1].toLowerCase();
       }
     },
     search() {
+      // throttle needed if extensive search
       console.log("search");
       this.formatSearchName();
       const searchViaName = this.customers.filter((customer) => {
@@ -63,13 +67,42 @@ export default {
         const nameValuesArr = Object.values(customer.name);
         // check if first/last exists
         if (
-          nameValuesArr.some((el) => el.toLowerCase() === this.firstName) ||
-          nameValuesArr.some((el) => el.toLowerCase() === this.lastName)
+          nameValuesArr.some((el) => el.toLowerCase() === this.name.first) ||
+          nameValuesArr.some((el) => el.toLowerCase() === this.name.last)
         )
           return customer;
       });
-      console.log(searchViaName);
-      this.foundCustomer = searchViaName;
+      this.selected.customer = searchViaName[0];
+    },
+    filterByCustomerId(arr) {
+      return arr.filter(
+        (item) => item.customerId === this.selected.customer.id
+      );
+    },
+    getPolicyData() {
+      let quotesUrl = "http://localhost:3000/quotes/";
+      let policesUrl = "http://localhost:3000/policies/";
+
+      const quotes = axios.get(quotesUrl);
+      const policies = axios.get(policesUrl);
+
+      axios
+        .all([quotes, policies])
+        .then(
+          axios.spread((...responses) => {
+            const quotesResponse = responses[0].data;
+            const policesResponse = responses[1].data;
+            this.selected.quote = this.filterByCustomerId(quotesResponse);
+            this.selected.policy = this.filterByCustomerId(policesResponse);
+          })
+        )
+        .catch((errors) => {
+          console.log(errors);
+          // react on errors.
+        });
+    },
+    getCustomerData() {
+      this.getPolicyData();
     },
   },
 };
